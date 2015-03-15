ff059ff192521b87c5708de621547ca1feed33bf
Mathias Gawlista <gawlista.mathias@vimn.com>
2015-03-15 08:58:52 +0100

    refs #3 Adds settings page for last.fm API.

---
+++ config/initializers/default_settings.rb
@@ -0,0 +1,12 @@
+if Setting.table_exists? && !Setting['home_page.general.plugins'].include?('home_page_music')
+  Setting['home_page.general.plugins'] = Setting['home_page.general.plugins'] + ['home_page_music']
+end
+
+Setting.defaults['home_page_music.general.available_apis'] = ['lastfm']
+Setting.defaults['home_page_music.general.enabled_apis'] = []
+
+# cannot be set to false yet for the time being without text logic
+Setting.defaults['home_page.apis.providers.lastfm.user_name'] = ''
+Setting.defaults['home_page.apis.providers.lastfm.charts.types.recent_tracks.is_image'] = true
+Setting.defaults['home_page.apis.providers.lastfm.charts.types.recent_tracks.design'] = 'TBM-RecentTracks2'
+Setting.defaults['home_page.apis.providers.lastfm.charts.types.recent_tracks.tracks_count'] = 6
