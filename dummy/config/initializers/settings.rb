31e230b9456066f4b5f89212c298b65a2cdea05a
Mathias Gawlista <gawlista.mathias@vimn.com>
2015-03-17 18:32:23 +0100

    refs #7 Adds page_modules to navigation.

--- dummy/config/initializers/settings.rb
+++ dummy/config/initializers/settings.rb
@@ -1,3 +1,3 @@
 if Setting.table_exists?
-  Setting['home_page.general.navigation.items'] = [['music', 'music'], 'users', 'settings', 'authentication']
+  Setting['home_page.general.navigation.items'] = [['music', 'music'], 'page_modules', 'users', 'settings', 'authentication']
 end
